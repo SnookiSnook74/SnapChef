995665bcd60f9133ddc5c013d9d85bd8e814e51f
DonHala <halapovaev@yandex.ru>
2025-01-31 11:10:13 +0500

    first

---
+++ SnapChef/Application/OpenAI.swift
@@ -0,0 +1,19 @@
+//
+//  OpenAI.swift
+//  SnapChef
+//
+//  Created by DonHalab on 31.01.2025.
+//
+
+import Foundation
+
+/// Структура для получения API ключа от OpenAi
+struct OpenAI {
+    static var apiKey: String {
+        if let key = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String {
+            return key
+        } else {
+            fatalError("API_KEY is not set in Info.plist.")
+        }
+    }
+}
---
+++ SnapChef/Application/SnapChefApp.swift
@@ -0,0 +1,32 @@
+//
+//  SnapChefApp.swift
+//  SnapChef
+//
+//  Created by DonHalab on 31.01.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SnapChefApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
